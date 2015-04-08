70b31884ed01e705b35f6c333f29bc2ef9849023
juspears <juspears@paypal.com>
2015-04-08 15:20:39 -0700

    Make checkbox true or false if no value is found

--- src/types/Checkbox.jsx
+++ src/types/Checkbox.jsx
@@ -6,9 +6,12 @@ var Checkbox = React.createClass({
     statics: {
         inputClassName: Constants.inputClassName
     },
+    doChange:function(e){
+        this.props.onValueChange(e.target.checked ? this.props.value || true : null, this.state.value || false, this.props.name, this.props.path);
+    },
     render() {
         var className = Constants.clz(Checkbox.inputClassName, this.props.editorClass);
-        return <input onBlur={this.handleValidate} onChange={this.handleChange} id={this.props.name}
+        return <input onBlur={this.handleValidate} onChange={this.doChange} id={this.props.name}
                       className={Checkbox.inputClassName} type="checkbox" value={this.state.value}
                       data-path={this.props.path}
                       title={this.props.title} placeholder={this.props.placeholder}/>
