1d218486609d84654fc0b72dde005f89d45c6aa5
juspears <juspears@paypal.com>
2015-09-11 01:35:57 -0700

    Added a nicer MM/YY to accept MM/20YY

--- test/types/Restricted-test.jsx
+++ test/types/Restricted-test.jsx
@@ -3,6 +3,9 @@ var TestUtils = require('react/lib/ReactTestUtils');
 var expect = require('expect');
 var Simulate = React.addons.TestUtils.Simulate;
 var Restricted = require('../../src/types/Restricted.jsx');
+function change(node, value) {
+    Simulate.change(node, {target: {value}});
+}
 describe('Checkbox', function () {
     this.timeout(30000);
     var Form = require('subschema').Form;
@@ -14,8 +17,66 @@ describe('Checkbox', function () {
 
     it('should create a restricted input', function () {
 
-        var root = into(<Restricted formatter="###-##" />);
+        var root = into(<Restricted formatter="###-##"/>);
         expect(root).toExist();
 
+    });
+    describe('mm20YY', function () {
+        var root = into(<Restricted formatter="mm20YY"/>);
+        expect(root).toExist();
+        var input = TestUtils.scryRenderedDOMComponentsWithTag(root, 'input')[0];
+        var inputEl = React.findDOMNode(input);
+
+        it('enters 2/16', function () {
+            change(input, '2');
+            expect(inputEl.value).toBe('02/');
+
+            change(input, '02/1');
+            expect(inputEl.value).toBe('02/201');
+
+            change(input, '02/2016');
+            expect(inputEl.value).toBe('02/16');
+        });
+
+        it('enters 1/16', function () {
+            change(input, '1');
+            expect(inputEl.value).toBe('1');
+
+            change(input, '1/');
+            expect(inputEl.value).toBe('01/');
+
+            change(input, '1/1');
+            expect(inputEl.value).toBe('01/201');
+
+            change(input, '01/16');
+            expect(inputEl.value).toBe('01/16');
+        });
+
+        it('enters 01/2016', function(){
+            change(input, '0');
+            expect(inputEl.value).toBe('0');
+
+            change(input, '01');
+            expect(inputEl.value).toBe('01/');
+
+            change(input, '01/2');
+            expect(inputEl.value).toBe('01/2');
+
+            change(input, '01/20');
+            expect(inputEl.value).toBe('01/20');
+
+            change(input, '01/201');
+            expect(inputEl.value).toBe('01/201');
+
+            change(input, '01/2016');
+            expect(inputEl.value).toBe('01/16');
+
+        });
+        it('enters 13', function(){
+
+            change(input, '13');
+            expect(inputEl.value).toBe('01/');
+
+        });
     });
 });
