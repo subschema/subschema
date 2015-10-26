f62534afbe0f9b0b6045c9e9dc8a4d12bdded157
jspears <speajus@gmail.com>
2015-10-25 22:47:44 -0700

    fix title

--- src/templates/EditorTemplate.jsx
+++ src/templates/EditorTemplate.jsx
@@ -18,12 +18,14 @@ var EditorTemplate = React.createClass({
     render(){
         var {name, title, help, errorClassName, message, fieldClass, loader, valueManager, children} = this.props;
         var error = this.state.error;
+        if (!title){ title = ''}
+
         return (<div
             className={style.group+" " + (error != null ? errorClassName || '' : '') + ' ' +  css.forEditor(this)}>
             <Content content={title} type="label" className={style.label} htmlFor={name}
                      valueManager={valueManager} loader={loader}/>
 
-            <div className="col-sm-10">
+            <div className={title ? "col-sm-10" : "col-sm-12"}>
                 {children}
                 <Content content={error ? error : help} key='error-block' type='p'
                          className={error ? style.error : style.help}
