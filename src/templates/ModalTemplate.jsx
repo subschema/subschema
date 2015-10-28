c632c2b231ff22846e8ebce6633614046d0d670d
juspears <juspears@paypal.com>
2015-10-28 16:32:24 -0700

    made modal external styles

--- src/templates/ModalTemplate.jsx
+++ src/templates/ModalTemplate.jsx
@@ -5,13 +5,13 @@ var Buttons = require('./ButtonsTemplate.jsx');
 var Content = require('../types/Content.jsx')
 var styles = require('../styles/ModalTemplate-style');
 var ValueManager = require('../ValueManager');
+var Editor = require('../Editor');
 var ModalTemplate = React.createClass({
     componentWillMount(){
         var {...value} = this.props.valueManager.path(this.props.path);
-        var copy = {
+        this.valueManager = ValueManager({
             [this.props.path]: value
-        }
-        this.valueManager = ValueManager(copy, this.props.valueManager.getErrors());
+        }, this.props.valueManager.getErrors());
     },
     handleClose(e){
         e && e.preventDefault();
@@ -30,26 +30,29 @@ var ModalTemplate = React.createClass({
         }
 
     },
+    renderFooter(buttons){
+        if (!buttons) return null;
+        return <div className={styles.footer}><Buttons buttons={buttons} onClick={this.handleBtnClose}/></div>
+    },
     render(){
         var {title, buttons, path,value, children, ...rest} = this.props;
         var cloneProps = {
             valueManager: this.valueManager
         }
-        return <div className={`modal ${styles.overlay}`} style={{display:'block'}}>
-            <div className="modal-backdrop fade in"></div>
-            <div className="modal-dialog" role="document" style={{zIndex:2000}}>
-                <div className="modal-content">
-                    <div className='modal-header'>
-                        <button onClick={this.handleClose} className='close' name={this.props.dismiss} value={value}
+        return <div className={`${styles.namespace} ${styles.overlay}`} style={{display:'block'}}>
+            <div className={styles.backdrop}></div>
+            <div className={styles.dialog} role="document" style={{zIndex:2000}}>
+                <div className={styles.content}>
+                    <div className={styles.header}>
+                        <button onClick={this.handleClose} className={styles.close} name={this.props.dismiss}
+                                value={value}
                                 aria-label="Close"><span aria-hidden="true">&times;</span></button>
                         {title ? <Content type='h4'  {...rest} content={title}/> : null }
                     </div>
-                    <div className='modal-body clearfix'>
+                    <div className={styles.body}>
                         {Children.map(children, (element)=>React.cloneElement(element, cloneProps))}
                     </div>
-                    <div className="modal-footer">
-                        { buttons ? <Buttons buttons={buttons} onClick={this.handleBtnClose}/> : null }
-                    </div>
+                    {this.renderFooter(buttons)}
                 </div>
             </div>
         </div>
