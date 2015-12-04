a2bbcc10371097adc5e9e5f9edb8efe7d6d466ac
juspears <juspears@paypal.com>
2015-12-03 19:09:24 -0800

    Exposing transition

--- src/index.jsx
+++ src/index.jsx
@@ -7,4 +7,7 @@ api.processors = require('./processors/index.js');
 api.types = require('./types/index.js');
 api.styles = require('./styles/index.js');
 api.Dom = require('./Dom');
+api.transitions = {
+    EventCSSTransitionGroup: require('./transition/EventCSSTransitionGroup.jsx')
+}
 module.exports = api;
