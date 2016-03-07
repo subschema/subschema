da20142c3a8974f3c4e309ac1448b22ffdbb16d2
juspears <juspears@paypal.com>
2016-03-06 16:52:33 -0800

    Fixed fieldAttrs

--- src/resolvers/fieldAttrs.js
+++ src/resolvers/fieldAttrs.js
@@ -2,30 +2,32 @@
 
 import {applyFuncs} from '../tutils';
 
+function handleAttrs(value, key) {
+    if (!value) return;
+    const keys = Object.keys(value);
+    for (let i = 0, l = keys.length; i < l; i++) {
+        const key = keys[i];
+        if (propKeys.indexOf(key) === -1) {
+            propKeys.push(key);
+        }
+        //This may be indeterminate, depending if something sets it later.
+        this.injected[key] = value[key];
+    }
+}
+
 export default function fieldAttrs(Clazz, key, propKeys) {
 
-    function handleAttrs(value) {
-        if (!value) return;
-        const keys = Object.keys(value);
-        for (let i = 0, l = keys.length; i < l; i++) {
-            const key = keys[i];
-            if (propKeys.indexOf(key) === -1) {
-                propKeys.push(key);
-            }
-            //This may be indeterminate, depending if something sets it later.
-            this.injected[key] = value[key];
-        }
-    }
+
 
     const ClazzP = Clazz.prototype;
 
     ClazzP.componentWillMount = applyFuncs(function () {
-        handleAttrs(this.props[key]);
+        this::handleAttrs(this.props[key], key);
     }, ClazzP.componentWillMount);
 
     ClazzP.componentWillReceiveProps = applyFuncs(function (newProps) {
         if (this.props[key] !== newProps[key]) {
-            handleAttrs(newProps[key]);
+            this::handleAttrs(newProps[key], key);
         }
     }, ClazzP.componentWillReceiveProps);
 }
