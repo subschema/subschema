454949de75038a37727b64e19d90e015c51871ea
Justin Spears <jspears@walmartlabs.com>
2017-04-17 15:46:31 -0700

    loader passes

---
+++ subschema-loader/test/loader-test.js
@@ -0,0 +1,26 @@
+import loaderFactory from '../lib/index';
+import expect from 'expect';
+function A() {
+}
+
+function B() {
+}
+
+describe('loadFactory', function () {
+
+    it('should create a loader', function () {
+        const loader = loaderFactory();
+        expect(loader).toExist();
+    });
+
+    it('should create a loader of loaders', function () {
+        const loader = loaderFactory();
+        loader.addTemplate({A});
+        const loader2 = loaderFactory([loader]);
+
+        loader.addTemplate('Be', B)
+        expect(loader2.loadTemplate('Be')).toBe(B);
+        expect(loader2.loadTemplate('A')).toBe(A);
+    })
+
+});
