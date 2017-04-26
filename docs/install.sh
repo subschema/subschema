6ab83f14a11812164da60d18bd3e667c5389c96c
Justin Spears <jspears@walmartlabs.com>
2017-04-25 17:00:21 -0700

    removed stuff

--- docs/install.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-for pkg in subschema-test-support subschema subschema-project subschema-demo; do
-  echo "installing $pkg";
-  (cd $pkg && npm install || echo "$pkg failed!">/dev/stderr);
-done
